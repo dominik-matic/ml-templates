a4a89eb71b1e779db0fcefe4a3f4925a61812327
Dominik Matić <dm51746@fer.hr>
2023-03-14 19:34:32 +0100

    Changed evaluator to only test data

--- standard/Evaluator.py
+++ standard/Evaluator.py
@@ -1,64 +1,32 @@
 class Evaluator:
 	def __init__(self,
 				criterion,
+				test_data,
 				num_classes,
-				valid_data,
-				test_data=None,
 				device='cpu',
-				save_path="models/model.pt",
-				verbose=True,
-				print_interval=10):
+				verbose=True):
 		self.criterion = criterion
-		self.num_classes = num_classes
-		self.valid_data = valid_data
 		self.test_data = test_data
+		self.num_classes = num_classes
 		self.device = device
-		self.save_path = save_path
 		self.verbose = verbose
-		self.print_interval = print_interval
 
-		self.valid_losses = []
-		self.best_valid_loss = None
-	
-	def _save_model(self, model):
-		if self.verbose:
-			print(f"Saving model to {self.save_path}...", end=" ")
-		torch.save(model.state_dict(), self.save_path)
-		if self.verbose:
-			print("DONE.")
-
-	def _epoch(self, model, data):
+	def _test_epoch(self, model):
 		model.eval()
 		metric = MulticlassConfusionMatrix(num_classes=self.num_classes)
 		losses = []
-		for i, X, Y in enumerate(data):
+		for i, X, Y in (pbar := tqdm(enumerate(self.test_data), desc="loss = ")):
 			with torch.no_grad():
 				y = model(X.to(self.device))
 				loss = self.criterion(Y.to(self.device), y)
 				losses.append(loss.item())
 				metric.update(Y, y.cpu())
-				if verbose and (i % self.print_interval == 0):
-					print(f"\tStep {i}:\tloss={losses[-1]}")
-		return metric, losses
+				pbar.set_description(f"loss = {losses[-1]}")
+		return sum(losses) / len(losses), metric
 
-	def validate(self, model):
-		if self.verbose:
-			print("Validating...")
-		metric, losses = _epoch(model, self.valid_data)
-		mean_valid_loss = np.mean(losses)
-		self.valid_losses.append(mean_valid_loss)
-		if self.best_valid_loss is None or mean_valid_loss < self.best_valid_loss:
-			self._save_model(model)
-			self.best_valid_loss = mean_valid_loss
-		return metric
-	
 	def test(self, model):
 		if self.verbose:
 			print("Testing...")
-		return _epoch(model, self.test_data)[0]
-		
-	def reset(self):
-		self.valid_losses = []
-		self.best_valid_loss = None
+		return _epoch(model)
 	
 	
