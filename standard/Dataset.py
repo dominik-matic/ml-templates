0e7da6ec43a3281f5dd69557af774cc34ec7ffe2
Dominik Matić <dm51746@fer.hr>
2023-03-15 19:42:12 +0100

    Added model and dataset templates

---
+++ standard/Dataset.py
@@ -0,0 +1,11 @@
+import torch
+
+class Dataset(torch.utils.data.Dataset):
+	def __init__(self):
+		pass
+
+	def __getitem__(self, index):
+		pass
+
+	def __len__(self):
+		pass
---
+++ standard/Model.py
@@ -0,0 +1,9 @@
+import torch
+
+class Model(torch.nn.Module):
+	def __init__(self):
+		self.name = "give your model a name"
+		pass
+
+	def forward(self, X):
+		pass
